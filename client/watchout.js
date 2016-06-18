f2c271a9bb91d3cb348a8319f97a73e530bdf53d
hcynnn216 <yuhongchao@gmail.com>
2016-06-17 18:52:26 -0700

    Enemies will now move around the map randomly

--- client/watchout.js
+++ client/watchout.js
@@ -35,9 +35,16 @@ var updateBestScore = function() {
   d3.select('.highscore').text('High Score: ' + gameStats.bestScore.toString());  
 };
 
+// class Player {
+//   constructor() {
+//     this.path
+//   }
+// }
+
 // Create the enemies data array and set random locations 
 // information for each enemy
 var createEnemies = function() {
+  console.log("creating enemies");
   return _.range(0, gameOptions.nEnemies).map(function(i) {
     return { 
       id: i,
@@ -47,26 +54,83 @@ var createEnemies = function() {
   });
 };
 
+var enemyData = createEnemies();
+
 // Render the game board
 var render = function(enemyData) {
-  var enemies = gameBoard.selectAll('circle.enemies')
-                  .data(enemyData, function(d) { return d.id; });
   
+  // DATA JOIN
+  var enemies = gameBoard.selectAll('circle.enemies')
+                  .data(enemyData);
+
+  // ENTER
   enemies.enter()
     .append('svg:circle')
       .attr('class', 'enemy')
       .attr('cx', function(enemy) { return axes.x(enemy.x); })
       .attr('cy', function(enemy) { return axes.y(enemy.y); })
       .attr('r', 10)
-      .attr('fill', 'red');
+      .attr('background-image', 'red');
+ 
+  // // UPDATE
+  // enemies.transition()
+  //          .duration(500)
+  //          .attr('cx', function(enemy) { return axes.x(Math.min(100, Math.max(0, enemy.x + Math.random() * 20 - 10))); })
+  //          .attr('cy', function(enemy) { return axes.y(Math.min(100, Math.max(0, enemy.y + Math.random() * 20 - 10))); });
 
+  // EXIT
   enemies.exit()
     .remove();
 };
 
+var update = function(enemyData) {
+  console.log('updating...');
+  gameBoard.selectAll('.enemy')
+           .data(enemyData)
+           .transition()
+             .duration(500)
+             .attr('cx', function(enemy) { return axes.x(Math.min(100, Math.max(0, enemy.x + Math.random() * 20 - 10))); })
+             .attr('cy', function(enemy) { return axes.y(Math.min(100, Math.max(0, enemy.y + Math.random() * 20 - 10))); });
+};
 
-// Create and render enemies
-render(createEnemies());
+render(enemyData);
+
+setInterval(function() {
+  update(enemyData);
+}, 500);
+
+// var startGame = function() {
+//   var enemyData = createEnemies();
+//   render(enemyData);
+  
+//   setInterval(function() { 
+//     update(enemyData);
+//   }, 1000);
+// };
+
+// startGame();
+
+// var startGame = function() {
+//   setInterval(function() {
+//     render();
+//     moveEnemies();
+//   }, 1000);
+// };
+
+// // Create and render enemies
+// 
+
+// var checkCollision = function(enemy, collidedCallback) {
+//   _.each()
+// };
+
+// var tweenWithCollisionDetection = function() {
+//   var enemy = d3.select(this);
+
+//   var startPos = {
+//     x: parseFloat
+//   }
+// }
 
 
 
